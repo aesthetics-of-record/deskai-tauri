a77dd01fafadc8a65b320b7a631a1f3842bcea8f
hyeonho cho <111675679+aesthetics-of-record@users.noreply.github.com>
2024-01-17 16:06:57 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { invoke } from '@tauri-apps/api/core';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
