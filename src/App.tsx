2141cc8888b3fedb13fc89f77549f74d4c334dc3
hyeonho cho <111675679+aesthetics-of-record@users.noreply.github.com>
2024-01-18 19:06:48 +0900

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { invoke } from '@tauri-apps/api/core';
+import { Button } from './components/ui/button';
+import WindowTitlebar from './components/titlebar/window-titlebar';
 
 function App() {
   return (
-    <div className='container'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </div>
+    <>
+      <WindowTitlebar />
+      <div className='container'>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        <Button>안녕</Button>
+      </div>
+    </>
   );
 }
 
--- src/components/titlebar/window-titlebar.tsx
+++ src/components/titlebar/window-titlebar.tsx
@@ -1,48 +1,27 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import {
-  isPermissionGranted,
-  requestPermission,
-  sendNotification,
-} from "@tauri-apps/api/notification";
+import { useEffect, useState } from 'react';
 
 const WindowTitlebar = () => {
   const [appWindow, setAppWindow] = useState<any>();
 
   // Import appWindow and save it inside the state for later usage
   async function setupAppWindow() {
-    const appWindow = (await import("@tauri-apps/api/window")).appWindow;
+    const appWindow = (await import('@tauri-apps/api/window')).appWindow;
     setAppWindow(appWindow);
-  }
-
-  async function notification() {
-    let permissionGranted = await isPermissionGranted();
-    if (!permissionGranted) {
-      const permission = await requestPermission();
-      permissionGranted = permission === "granted";
-    }
-    console.log(permissionGranted);
-    if (permissionGranted) {
-      sendNotification({
-        title: "deskai",
-        body: "어플리케이션이 실행되었습니다 !",
-      });
-    }
   }
 
   useEffect(() => {
     setupAppWindow();
-    notification();
   }, []);
 
   return (
-    <div data-tauri-drag-region className="flex p-2 justify-between ">
-      <div className="font-bold"></div>
+    <div data-tauri-drag-region className='flex p-2 justify-between '>
+      <div className='font-bold'></div>
 
-      <div className="flex gap-x-2">
+      <div className='flex gap-x-2'>
         <div
-          className="rounded-full flex justify-center cursor-pointer"
+          className='rounded-full flex justify-center cursor-pointer'
           onClick={() => {
             appWindow.minimize();
           }}
@@ -50,7 +29,7 @@ const WindowTitlebar = () => {
           -
         </div>
         <div
-          className="rounded-full flex items-center justify-center cursor-pointer"
+          className='rounded-full flex items-center justify-center cursor-pointer'
           onClick={() => {
             appWindow.toggleMaximize();
           }}
@@ -58,7 +37,7 @@ const WindowTitlebar = () => {
           +
         </div>
         <div
-          className="rounded-full flex items-center justify-center cursor-pointer"
+          className='rounded-full flex items-center justify-center cursor-pointer'
           onClick={() => {
             appWindow.hide();
           }}
